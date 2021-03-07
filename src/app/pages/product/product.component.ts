10bbb3ce372d4ea88cd895987bc6107391ae3b16
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-03-07 02:02:42 +0000

    add

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+	selector: "app-product",
+	templateUrl: "./product.component.html",
+	styleUrls: ["./product.component.css"],
 })
 export class ProductComponent implements OnInit {
+	productForm: FormGroup;
+	proView: boolean = true;
 
-  constructor() { }
+  // viewchnge() {
+  //   this.proView = false;
+  // }
 
-  ngOnInit(): void {
-  }
 
+	constructor(private fb: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.productForm = this.fb.group({
+			Name: ["", [Validators.required]],
+			Category: ["", [Validators.required]],
+			Price: [[Validators.required]],
+			Description: ["", [Validators.required]],
+			image: ["", [Validators.required]],
+		});
+	}
 }
+
