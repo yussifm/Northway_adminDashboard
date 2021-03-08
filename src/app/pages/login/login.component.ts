74dcd0493846b712b13a5b6bbe6f1f0e60553d70
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2021-03-08 22:09:01 +0000

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,14 +9,23 @@ import { Router } from "@angular/router";
 	styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-	constructor( private router: Router,private fb: FormBuilder) {}
+	loginForm: FormGroup;
+	gotoMain() {
+		this.router.navigate(["main"]);
+	}
+	constructor(private router: Router, private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email, Validators.pattern('[a-z0-9.@]*')] ],
-      password: ['', [ Validators.required, Validators.min(8)]]
-
-    })
-  }
+	ngOnInit(): void {
+		this.loginForm = this.fb.group({
+			email: [
+				"",
+				[
+					Validators.required,
+					Validators.email,
+					Validators.pattern("[a-z0-9.@]*"),
+				],
+			],
+			password: ["", [Validators.required, Validators.min(8)]],
+		});
+	}
 }
